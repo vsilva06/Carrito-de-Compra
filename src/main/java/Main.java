435c4d863a758acea28864e07c0db89a79425849
Vicente <v.silva06@ufromail.cl>
2020-11-10 10:03:03 -0300

    Mostrar carrito

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,13 @@ public class Main {
         int[] arreglo = new int[largo];
 
         llenarCarrito(arreglo);
+        mostrarCarrito(arreglo);
+    }
+
+    private static void mostrarCarrito(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("["+i+"] "+arreglo[i]+" ");
+        }
     }
 
     private static int[] llenarCarrito(int[] arreglo) {
@@ -19,6 +26,7 @@ public class Main {
         for (int i = 0; i < arreglo.length; i++) {
             nProductos = rNum.nextInt(16);
             arreglo[i] = nProductos;
+
         }
         return arreglo;
     }
